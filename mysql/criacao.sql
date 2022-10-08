bb39485f90ff69d0fcb31e6bdc47b2826b68a6d3
MaykoDiouzef <maykodiouzef@gmail.com>
2022-10-07 23:32:31 -0300

    att bd e add dados bd
    Co-Authored-By: Wagner012 <31023039+Wagner012@users.noreply.github.com>

--- mysql/criacao.sql
+++ mysql/criacao.sql
@@ -1,134 +1,241 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: quizestatistico
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema quizestatistico
--- -----------------------------------------------------
+--
+-- Table structure for table `administradores`
+--
 
--- -----------------------------------------------------
--- Schema quizestatistico
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `quizestatistico` DEFAULT CHARACTER SET utf8 ;
-USE `quizestatistico` ;
+DROP TABLE IF EXISTS `administradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administradores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`niveis`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`niveis` (
-  `id` INT NOT NULL,
-  `nivel` VARCHAR(45) NOT NULL COMMENT 'Representa o nível de dificuldade que vai ter na pergunta',
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `administradores`
+--
 
+LOCK TABLES `administradores` WRITE;
+/*!40000 ALTER TABLE `administradores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `administradores` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`cursos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`cursos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `curso` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `cursos`
+--
 
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `curso` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`questoes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`questoes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nivel` INT NOT NULL COMMENT 'Representa o nível de dificuldade que vai ter na pergunta',
-  `curso` INT NOT NULL,
-  `pergunta` TEXT NOT NULL,
-  `resposta_certa` TEXT NOT NULL,
-  `resposta_errada1` TEXT NOT NULL,
-  `resposta_errada2` TEXT NOT NULL,
-  `resposta_errada3` TEXT NOT NULL,
-  `explicacao` TEXT NOT NULL,
-  `imagem` VARCHAR(255) NULL,
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'Licenciatura em Ciências Biológicas'),(2,'Bacharelado em Sistemas de Informação'),(3,'Licenciatura em Química'),(4,'Bacharelado em Zootecnia'),(5,'Bacharelado em Agronomia');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `niveis`
+--
+
+DROP TABLE IF EXISTS `niveis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `niveis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nivel` varchar(45) NOT NULL COMMENT 'Representa o nível de dificuldade que vai ter na pergunta',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `niveis`
+--
+
+LOCK TABLES `niveis` WRITE;
+/*!40000 ALTER TABLE `niveis` DISABLE KEYS */;
+INSERT INTO `niveis` VALUES (1,'Fácil'),(2,'Médio'),(3,'Difícil');
+/*!40000 ALTER TABLE `niveis` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `perguntas_quiz`
+--
+
+DROP TABLE IF EXISTS `perguntas_quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `perguntas_quiz` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `questao` int NOT NULL,
+  `quiz` int NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_pergunta_nivel1_idx` (`nivel` ASC) VISIBLE,
-  INDEX `fk_pergunta_curso1_idx` (`curso` ASC) VISIBLE,
-  CONSTRAINT `fk_pergunta_nivel1`
-    FOREIGN KEY (`nivel`)
-    REFERENCES `quizestatistico`.`niveis` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pergunta_curso1`
-    FOREIGN KEY (`curso`)
-    REFERENCES `quizestatistico`.`cursos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_quiz_questao1_idx` (`questao`),
+  KEY `fk_quiz_nomequiz1_idx` (`quiz`),
+  CONSTRAINT `fk_quiz_nomequiz1` FOREIGN KEY (`quiz`) REFERENCES `quizzes` (`id`),
+  CONSTRAINT `fk_quiz_questao1` FOREIGN KEY (`questao`) REFERENCES `questoes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `perguntas_quiz`
+--
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`ranks`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`ranks` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(150) NOT NULL,
-  `pontuacao` INT NOT NULL,
-  `acerto` INT NOT NULL,
-  `erro` INT NOT NULL,
-  `curso` INT NOT NULL,
+LOCK TABLES `perguntas_quiz` WRITE;
+/*!40000 ALTER TABLE `perguntas_quiz` DISABLE KEYS */;
+/*!40000 ALTER TABLE `perguntas_quiz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questoes`
+--
+
+DROP TABLE IF EXISTS `questoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questoes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nivel` int NOT NULL COMMENT 'Representa o nível de dificuldade que vai ter na pergunta',
+  `curso` int NOT NULL,
+  `tema` int NOT NULL,
+  `pergunta` text NOT NULL,
+  `resposta_certa` text NOT NULL,
+  `resposta_errada1` text NOT NULL,
+  `resposta_errada2` text NOT NULL,
+  `resposta_errada3` text NOT NULL,
+  `explicacao` text NOT NULL,
+  `imagem` blob,
   PRIMARY KEY (`id`),
-  INDEX `fk_hanking_curso1_idx` (`curso` ASC) VISIBLE,
-  CONSTRAINT `fk_hanking_curso1`
-    FOREIGN KEY (`curso`)
-    REFERENCES `quizestatistico`.`cursos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_pergunta_nivel1_idx` (`nivel`),
+  KEY `fk_pergunta_curso1_idx` (`curso`),
+  KEY `fk_questoes_temas1_idx` (`tema`),
+  CONSTRAINT `fk_pergunta_curso1` FOREIGN KEY (`curso`) REFERENCES `cursos` (`id`),
+  CONSTRAINT `fk_pergunta_nivel1` FOREIGN KEY (`nivel`) REFERENCES `niveis` (`id`),
+  CONSTRAINT `fk_questoes_temas1` FOREIGN KEY (`tema`) REFERENCES `temas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `questoes`
+--
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`quizzes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`quizzes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome_quiz` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `questoes` WRITE;
+/*!40000 ALTER TABLE `questoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questoes` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `quizzes`
+--
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`perguntas_quiz`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`perguntas_quiz` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `questao` INT NOT NULL,
-  `quizzes` INT NOT NULL,
+DROP TABLE IF EXISTS `quizzes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quizzes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_quiz` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+LOCK TABLES `quizzes` WRITE;
+/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranks`
+--
+
+DROP TABLE IF EXISTS `ranks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ranks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `pontuacao` int NOT NULL,
+  `acerto` int NOT NULL,
+  `erro` int NOT NULL,
+  `curso` int NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_quiz_questao1_idx` (`questao` ASC) VISIBLE,
-  INDEX `fk_quiz_nomequiz1_idx` (`quizzes` ASC) VISIBLE,
-  CONSTRAINT `fk_quiz_questao1`
-    FOREIGN KEY (`questao`)
-    REFERENCES `quizestatistico`.`questoes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_quiz_nomequiz1`
-    FOREIGN KEY (`quizzes`)
-    REFERENCES `quizestatistico`.`quizzes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_hanking_curso1_idx` (`curso`),
+  CONSTRAINT `fk_hanking_curso1` FOREIGN KEY (`curso`) REFERENCES `cursos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `ranks`
+--
 
--- -----------------------------------------------------
--- Table `quizestatistico`.`administradores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `quizestatistico`.`administradores` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(150) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `senha` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `ranks` WRITE;
+/*!40000 ALTER TABLE `ranks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranks` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `temas`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `temas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `temas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tema` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temas`
+--
+
+LOCK TABLES `temas` WRITE;
+/*!40000 ALTER TABLE `temas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `temas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-07 21:49:58
