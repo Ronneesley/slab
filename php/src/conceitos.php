ad675ca1bfac49811281bf7c90b0480ca15eaa01
Leonardo Maia <leof.maia88@gmail.com>
2022-10-30 16:06:07 -0300

    #36 - Layout html;  #55 - Página principal responsiva;  #56 - Menu da parte do usuário do layout

---
+++ php/src/conceitos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>Conceitos</title>
+	  <link rel="stylesheet" href="conceitos.css">
+
+	</head>
+	<body id="conceitos_body">
+
+
+		<ul id="ul_conceitos">
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Estatística Descritiva</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">População</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Amostra</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Dado x Variável</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Variável Resposta</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Tabelas</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Distribuição de Frequências</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Gráficos</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Medidas Descritivas</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Medidas Separatrizes</a></li>
+			<li class="li_conceitos"><a class="a_conceitos" href="#">Medidas de Disperção</a></li>
+		</ul>
+	
+    </body>
+
+</html>
+
+
