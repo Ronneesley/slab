bece149814568eb17cbe1e3b51a353a3a36be380
Leonardo Maia <leof.maia88@gmail.com>
2022-11-04 16:33:45 -0300

    Issue #54 - Função que processa texto com marcações de imagens

---
+++ php/src/quiz/util/ProcessadorTexto.php
@@ -0,0 +1,17 @@
+<?php
+
+class ProcessadorTexto {    
+	
+    public function processar_marcacoes_de_imagens($string) {
+    	
+		$string2 = str_replace("[img id=\"", "<img src=\"miniatura.php?id=", $string);
+		$string3 = str_replace("\" largura=\"","&largura=", $string2);
+		$string4 = str_replace("\" altura=\"","&altura=", $string3);
+		$string_final =  str_replace("\"]"," />", $string4);
+		
+		return $string_final;
+	}
+    
+}
+
+?>
