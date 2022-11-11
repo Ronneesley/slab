4c9234b790d8f1bbaf2dfddb922c37779e535ddb
Ronneesley <ronneesley@gmail.com>
2022-11-11 20:11:19 -0300

    Instalação completa

---
+++ bin/abrir.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+google-chrome http://localhost/quizestatistico/php/src
---
+++ bin/instalar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./instalacao_php.sh
+./instalacao_netbeans.sh
+./instalacao_mysql_workbench.sh
+./instalacao_composer.sh
+
+cd ../php/src
+composer install
