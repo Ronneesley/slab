dc496106611ca26a9fc5699650809fdd116ae1d0
faculdadeifgoiano <114781798+faculdadeifgoiano@users.noreply.github.com>
2022-12-07 14:59:43 -0300

    Designer Perguntas

---
+++ scripts_R/medidas_tendencia_central/media.R
@@ -0,0 +1,5 @@
+#Script em R realizado para calcular a média aritmética de um determinado conjunto de dados.
+
+conjunto <- c(3,6,7)
+
+mean(conjunto)
---
+++ scripts_R/medidas_tendencia_central/mediana.R
@@ -0,0 +1,5 @@
+#Script em R realizado para calcular a mediana de um determinado conjunto de dados.
+
+conjunto <- c(5,4,3,1,1,5)
+
+median(conjunto)
