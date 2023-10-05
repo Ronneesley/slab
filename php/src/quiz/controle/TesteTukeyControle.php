f87cec8a16d5eae856efc64ae3843cc91c69d34f
isaqwe <isaquepontes47@gmail.com>
2023-10-05 12:59:30 -0300

    reciclando codigo anova do dbc para o tukey

--- php/src/quiz/controle/TesteTukeyControle.php
+++ php/src/quiz/controle/TesteTukeyControle.php
@@ -1,6 +1,8 @@
 <?php
 namespace QuizEstatistico\controle;
 
+use QuizEstatistico\modelo\TesteTukey;
+
 /**
  * Controle para o teste de Tukey
  * @author Ronneesley Moura Teles
@@ -12,6 +14,21 @@ class TesteTukeyControle extends ControleBase {
             case "novo_teste":
                 $this->novoTeste();
                 break;
+            case "novo":
+                $this->mostrarConfiguracaoInicial();
+                break;
+            case "montar_quadro":
+                $this->montarQuadro();
+                break;
+            case "calcular":
+                $this->calcular();
+                break;
+            case "salvar_dados":
+                $this->salvarDados();
+                break;
+            case "importar":
+                $this->importar();
+                break;        
             
         }
     }
@@ -23,5 +40,87 @@ class TesteTukeyControle extends ControleBase {
             [ "letra" => chr(ord('a') + 1) ]);
     }
 
+    public function mostrarConfiguracaoInicial(){
+        $layout = $this->configurarTemplate("layout.html");
+        $this->mostrarPaginaLayout($layout, "calculadora/teste_tukey/novo.html");
+    }
+
+    public function montarQuadro(){
+        $I = $_POST["n_tratamentos"];
+        $J = $_POST["n_blocos"];
+
+        $layout = $this->configurarTemplate("layout.html");
+        $this->mostrarPaginaLayout($layout, "calculadora/teste_tukey/quadro.html", 
+            [ "I" => $I, "J" => $J ]);
+    }
+
+    function formatarNumero($numero, $digitos = 2){
+        return strtr( number_format($numero, $digitos) , ".", ",");
+    }
+
+    public function calcular(){    
+        $tratamentos = $_REQUEST["tratamentos"];
+        $leiturasString = $_REQUEST["leituras"];        
+        $leituras = $this->formatarLeituras($leiturasString);
+        
+        $J = $_REQUEST["n_blocos"];        
+    
+        $this->calcularTukey($tratamentos, $leituras, $J);
+    }
+
+    public function calcularTukey($tratamentos, $leituras, $J){
+        $tukey = new TesteTukey();
+        $tukey->calcular($tratamentos, $leituras, $J);
+
+        $layout = $this->configurarTemplate("layout.html");
+        $this->mostrarPaginaLayout($layout, "calculadora/teste_tukey/tukey.html", 
+            [ "tukey" => $tukey ]);
+    }
+
+    function formatarLeituras($leiturasString){
+        $leituras = array();
+        for ($i = 0; $i < count($leiturasString); $i++){
+            $ts = array();
+            
+            for ($j = 0; $j < count($leiturasString[$i]); $j++){                
+                $valorConvertido = floatval(str_replace(",", ".", $leiturasString[$i][$j]));
+                
+                array_push($ts, $valorConvertido);
+            }
+            
+            array_push($leituras, $ts);
+        }
+
+        return $leituras;
+    }
+
+    public function salvarDados(){
+        $I = intval($_REQUEST["I"]);
+        $J = intval($_REQUEST["J"]);
+        $tratamentos = $_REQUEST["tratamentos"];
+        $leiturasString = $_REQUEST["leituras"];
+        $leituras = $this->formatarLeituras($leiturasString);
+
+        $json = array(
+            "I" => $I,
+            "J" => $J,
+            "tratamentos" => $tratamentos,
+            "leituras" => $leituras
+        );
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/json');
+        header('Content-Disposition: attachment; filename="dados.json"');
+        echo json_encode($json, JSON_PRETTY_PRINT);
+    }
+
+    public function importar(){
+        $path = $_FILES["arquivo"]["tmp_name"];
+
+        $json = json_decode(file_get_contents($path));
+        
+        $this->calcularTukey($json->tratamentos, $json->leituras, $json->J);
+    }
 }
+
 ?>
--- php/src/quiz/modelo/TesteTukey.php
+++ php/src/quiz/modelo/TesteTukey.php
@@ -6,6 +6,309 @@ namespace QuizEstatistico\modelo;
  * @author Ronneesley Moura Teles
  */
 class TesteTukey {
+
+    private $tratamentos;
+    
+    private $leituras;
+
+    private $I;
+
+    private $J;
+
+    private $K;
+
+    private $C;
+
+    private $L;
+
+    private $G;
+
+    private $GLTrat;
+    
+    private $SQTrat;
+
+    private $QMTrat;
+
+    private $SQBloco;
+
+    private $GLBloco;
+
+    private $QMBloco;
+
+    private $FBlocoCalc;
+
+    private $FCalc;
+
+    private $GLRes;
+
+    private $SQRes;
+
+    private $QMRes;
+
+    private $GLTotal;
+
+    private $SQTotal;
+
+    private $conclusao;
+
+    private $alfa;
+
+    private $FTab;
+
+    function converterNumero($numero_em_string){
+        return floatval(strtr($numero_em_string, ",", "."));
+    }
+
+    public function calcular($tratamentos, $leituras, $J){
+        $this->J = $J;
+        $this->tratamentos = $tratamentos;
+        $this->leituras = $leituras;
+
+        $this->I = count($tratamentos);
+
+        $this->L = array();
+        for ($i = 0; $i < $this->I; $i++){
+            $S = 0;
+            for ($j = 0; $j < $J; $j++){
+                $S += $this->converterNumero($leituras[$i][$j]);
+            }
+    
+            $this->L[$i] = $S;
+        }
+    
+        $this->G = 0;
+        for ($i = 0; $i < $this->I; $i++){
+            $this->G += $this->L[$i];
+        }
+    
+        //Calculando o valor K
+        $this->K = $this->G ** 2 / ($this->I * $this->J);
+   
+        //Calculando SQTotal
+        $S = 0;
+        for ($i = 0; $i < $this->I; $i++){
+            for ($j = 0; $j < $this->J; $j++){
+                $S += $this->converterNumero($leituras[$i][$j]) ** 2;
+            }
+        }
+    
+        $this->SQTotal = $S - $this->K;
+    
+        //Calculando SQTrat
+        $S = 0;
+        for ($i = 0; $i < $this->I; $i++){
+            $S += $this->L[$i] ** 2;
+        }
+    
+        $this->SQTrat = $S / $this->J - $this->K;
+
+        //Calcula os valores calculados de F
+        $this->FBlocoCalc = $this->QMBloco / $this->QMRes;    
+        $this->FCalc = $this->QMTrat / $this->QMRes;
+
+        $this->alfa = 0.05; //Nível de significância
+
+        $this->FTab = 3.16; //Trabalho a ser feito
+
+        if ($this->FCalc < $this->FTab){
+            $this->conclusao = "O teste é não significativo ao nível de significância de $this->alfa. Aceitamos H0.";
+        } else {
+            $this->conclusao = "O teste é significativo ao nível de significância de $this->alfa. Rejeita H0 em Favor de H1.";
+        }
+    }
+    
+    public function getI() {
+        return $this->I;
+    }
+
+    public function getJ() {
+        return $this->J;
+    }
+
+    public function getL() {
+        return $this->L;
+    }
+
+    public function getG() {
+        return $this->G;
+    }
+
+    public function getGLTrat() {
+        return $this->GLTrat;
+    }
+
+    public function getSQTrat() {
+        return $this->SQTrat;
+    }
+
+    public function getQMTrat() {
+        return $this->QMTrat;
+    }
+
+    public function getFCalc() {
+        return $this->FCalc;
+    }
+
+    public function getGLRes() {
+        return $this->GLRes;
+    }
+
+    public function getSQRes() {
+        return $this->SQRes;
+    }
+
+    public function getQMRes() {
+        return $this->QMRes;
+    }
+
+    public function getGLTotal() {
+        return $this->GLTotal;
+    }
+
+    public function getSQTotal() {
+        return $this->SQTotal;
+    }
+
+    public function getConclusao() {
+        return $this->conclusao;
+    }
+
+    public function setI($I): void {
+        $this->I = $I;
+    }
+
+    public function setJ($J): void {
+        $this->J = $J;
+    }
+
+    public function setL($L): void {
+        $this->L = $L;
+    }
+
+    public function setG($G): void {
+        $this->G = $G;
+    }
+
+    public function setGLTrat($GLTrat): void {
+        $this->GLTrat = $GLTrat;
+    }
+
+    public function setSQTrat($SQTrat): void {
+        $this->SQTrat = $SQTrat;
+    }
+
+    public function setQMTrat($QMTrat): void {
+        $this->QMTrat = $QMTrat;
+    }
+
+    public function setFCalc($FCalc): void {
+        $this->FCalc = $FCalc;
+    }
+
+    public function setGLRes($GLRes): void {
+        $this->GLRes = $GLRes;
+    }
+
+    public function setSQRes($SQRes): void {
+        $this->SQRes = $SQRes;
+    }
+
+    public function setQMRes($QMRes): void {
+        $this->QMRes = $QMRes;
+    }
+
+    public function setGLTotal($GLTotal): void {
+        $this->GLTotal = $GLTotal;
+    }
+
+    public function setSQTotal($SQTotal): void {
+        $this->SQTotal = $SQTotal;
+    }
+
+    public function setConclusao($conclusao): void {
+        $this->conclusao = $conclusao;
+    }
+    
+    public function getK() {
+        return $this->K;
+    }
+
+    public function setK($K): void {
+        $this->K = $K;
+    }
+    
+    public function getAlfa() {
+        return $this->alfa;
+    }
+
+    public function getFTab() {
+        return $this->FTab;
+    }
+
+    public function setAlfa($alfa): void {
+        $this->alfa = $alfa;
+    }
+
+    public function setFTab($FTab): void {
+        $this->FTab = $FTab;
+    }
+    
+    public function getTratamentos() {
+        return $this->tratamentos;
+    }
+
+    public function getLeituras() {
+        return $this->leituras;
+    }
+
+    public function setTratamentos($tratamentos): void {
+        $this->tratamentos = $tratamentos;
+    }
+
+    public function setLeituras($leituras): void {
+        $this->leituras = $leituras;
+    }
+
+    public function getC(){
+        return $this->C;
+    }
+
+    public function setC($C){
+        $this->C = $C;
+    }
+
+    public function getSQBloco(){
+        return $this->SQBloco;
+    }
+
+    public function setSQBloco($SQBloco){
+        $this->SQBloco = $SQBloco;
+    }
+
+    public function getGLBloco(){
+        return $this->GLBloco;
+    }
+
+    public function setGLBloco($GLBloco){
+        $this->GLBloco = $GLBloco;
+    }
+
+    public function getQMBloco(){
+        return $this->QMBloco;
+    }
+
+    public function setQMBloco($QMBloco){
+        $this->QMBloco = $QMBloco;
+    }
+
+    public function getFBlocoCalc(){
+        return $this->FBlocoCalc;
+    }
+
+    public function setFBlocoCalc($FBlocoCalc){
+        $this->FBlocoCalc = $FBlocoCalc;
+    }
+
     /**
      * Calcula a diferença mínima significativa de Tukey
      */
