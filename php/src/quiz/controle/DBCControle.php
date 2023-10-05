c4d00c8a71da54535b557c99f773cd524af9e165
Ronneesley <ronneesley@gmail.com>
2023-10-04 22:12:58 -0300

    DBC completo

--- php/src/quiz/controle/DBCControle.php
+++ php/src/quiz/controle/DBCControle.php
@@ -20,6 +20,12 @@ class DBCControle extends ControleBase {
             case "calcular":
                 $this->calcular();
                 break;
+            case "salvar_dados":
+                $this->salvarDados();
+                break;
+            case "importar":
+                $this->importar();
+                break;
         }
     }
 
@@ -43,29 +49,66 @@ class DBCControle extends ControleBase {
 
     public function calcular(){    
         $tratamentos = $_REQUEST["tratamentos"];
-        $leiturasString = $_REQUEST["leituras"];
-        
-        $leituras = array();
-        for ($i = 0; $i < count($leiturasString); $i++){
-            $ts = array();
-            
-            for ($j = 0; $j < count($leiturasString[$i]); $j++){                
-                $valorConvertido = str_replace(",", ".", $leiturasString[$i][$j]);
-                
-                array_push($ts, $valorConvertido);
-            }
-            
-            array_push($leituras, $ts);
-        }
+        $leiturasString = $_REQUEST["leituras"];        
+        $leituras = $this->formatarLeituras($leiturasString);
         
         $J = $_REQUEST["n_blocos"];        
     
+        $this->calcularDBC($tratamentos, $leituras, $J);
+    }
+
+    public function calcularDBC($tratamentos, $leituras, $J){
         $dbc = new DBC();
         $dbc->calcular($tratamentos, $leituras, $J);
 
         $layout = $this->configurarTemplate("layout.html");
         $this->mostrarPaginaLayout($layout, "dbc/dbc.html", 
             [ "dbc" => $dbc ]);
+    }
+
+    function formatarLeituras($leiturasString){
+        $leituras = array();
+        for ($i = 0; $i < count($leiturasString); $i++){
+            $ts = array();
+            
+            for ($j = 0; $j < count($leiturasString[$i]); $j++){                
+                $valorConvertido = floatval(str_replace(",", ".", $leiturasString[$i][$j]));
+                
+                array_push($ts, $valorConvertido);
+            }
+            
+            array_push($leituras, $ts);
+        }
+
+        return $leituras;
+    }
+
+    public function salvarDados(){
+        $I = intval($_REQUEST["I"]);
+        $J = intval($_REQUEST["J"]);
+        $tratamentos = $_REQUEST["tratamentos"];
+        $leiturasString = $_REQUEST["leituras"];
+        $leituras = $this->formatarLeituras($leiturasString);
+
+        $json = array(
+            "I" => $I,
+            "J" => $J,
+            "tratamentos" => $tratamentos,
+            "leituras" => $leituras
+        );
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/json');
+        header('Content-Disposition: attachment; filename="dados.json"');
+        echo json_encode($json, JSON_PRETTY_PRINT);
+    }
+
+    public function importar(){
+        $path = $_FILES["arquivo"]["tmp_name"];
+
+        $json = json_decode(file_get_contents($path));
+        
+        $this->calcularDBC($json->tratamentos, $json->leituras, $json->J);
     }
 }
 
