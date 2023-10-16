2e0263ace51b6167d0cffa2695bc52d96ecc45c3
Ronneesley <ronneesley@gmail.com>
2023-10-16 20:07:17 -0300

    Correção instalador dev

--- bin/configurar_ambiente_dev.sh
+++ bin/configurar_ambiente_dev.sh
@@ -2,21 +2,24 @@
 
 source 'utilitarios.sh'
 
-#Entra dentro da pasta php e instala as dependências do npm e do composer
-cd ../php/src
+#Armazena a pasta bin
+BIN=`pwd`
 
 #NPM
 echo "###########################################"
 echo -n "Verificando se o NPM está instalado: "
 npm --version
-RESULTADO=$?
+RESULTADO=1
 echo "###########################################"
 
 if [ $RESULTADO -ne 0 ]; then
+    cd $BIN
     echo "Iniciando a instalação do NPM primeiro: "
     sudo ./instalar_ferramenta.sh node
 fi
 
+#Entra dentro da pasta php e instala as dependências do npm e do composer
+cd ../php/src
 npm install
 verificar_operacao
 
@@ -28,10 +31,12 @@ RESULTADO=$?
 echo "###########################################"
 
 if [ $RESULTADO -ne 0 ]; then
+    cd $BIN
     echo "Iniciando a instalação do Composer primeiro: "
     sudo ./instalar_ferramenta.sh composer
 fi
 
+cd ../php/src
 composer install
 verificar_operacao
 
@@ -53,6 +58,7 @@ RESULTADO=$?
 echo "###########################################"
 
 if [ $RESULTADO -ne 0 ]; then
+    cd $BIN
     echo "Iniciando a instalação do Docker primeiro: "
     sudo ./instalar_ferramenta.sh docker
 fi
