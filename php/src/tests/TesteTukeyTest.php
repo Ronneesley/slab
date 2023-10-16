68d41b287ca06acb4d75cc6f1bd34517859e6326
bielvitooor <gabrielvitorsilvabrito@gmail.com>
2023-10-16 19:52:19 -0300

    testes de tukey

--- php/src/tests/TesteTukeyTest.php
+++ php/src/tests/TesteTukeyTest.php
@@ -28,39 +28,52 @@ final class TesteTukeyTest extends TestCase
         $this->assertEquals(array('c'), $r[3]);
     }
 
-    /*public function testExemploVideoA(): void
+     /**
+     * Comparação de variedades de leite
+     * 
+     * Fonte:
+     * https://www.youtube.com/watch?v=zV7ToVXQCBA 
+     * 
+     */
+    public function testeLeite(): void
     {
         $t = new TesteTukey();
-        $delta = $t->calcularDelta(3.51, 0.64,10);
+        $delta = $t->calcularDelta(4.05,0.9988,5);
+
+        $this->assertEqualsWithDelta(1.81, $delta, 0.01);
+        $medias=[22,20,19,18.5];
+        list($r, $nC) = $t->classificar($medias,1.81);
+        
+        
+        $this->assertEquals(array('a'), $r[0]);
+        $this->assertEquals(array('b'), $r[1]);
+        $this->assertEquals(array('b'), $r[2]);
+        $this->assertEquals(array('b'), $r[3]);    
+        print_r($r);
+    }
+
+    /**
+     * Comparação Satisfaçaõo de posto de trabalho
+     * Fonte:
+     * 
+     *https://www.youtube.com/watch?v=qnpP3-sP6Uc teste OK!
+     * 
+     * 
+     */
+    public function testTrabalho(): void
+    {
+        $t = new TesteTukey();
+        $delta = $t->calcularDelta(3.51,0.64,10);
 
         $this->assertEqualsWithDelta(0.888, $delta, 0.01);
-
-        $r = $t->classificar([7.3,5.9,8.6],0.888);
-        
+        $medias=[7.3,5.9,8.6];
+        list($r,$nC)= $t->classificar($medias,0.888);
         
         $this->assertEquals(array('a'), $r[0]);
         $this->assertEquals(array('b'), $r[1]);
         $this->assertEquals(array('c'), $r[2]);
-        //$this->assertEquals(array('b'), $r[3]);        
         print_r($r);
-    }*/
-
-    //https://www.youtube.com/watch?v=zV7ToVXQCBA teste OK!
-    //https://www.youtube.com/watch?v=qnpP3-sP6Uc teste OK!
-    /*public function testExemploVideo2(): void
-    {
-        $t = new TesteTukey();
-        $delta = $t->calcularDelta(4.20,0.5883,4);
-
-        $this->assertEqualsWithDelta(1.61, $delta, 0.01);
-
-        $r = $t->classificar([11.2,9.3,8.2,7.9],1.61);
-        
-
-        $this->assertEquals(array('a'), $r[0]);
-        $this->assertEquals(array('b'), $r[1]);
-        $this->assertEquals(array('b'), $r[2]);
-        $this->assertEquals(array('b'), $r[3]);        
-    }*/
+       
+    }
 }
 ?> 
