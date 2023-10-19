badf4d96c5f298f00f05bc6d221925f7eec9cf0c
Ronneesley <ronneesley@gmail.com>
2023-10-18 22:11:16 -0300

    Edições do docker de Ubuntu

--- bin/slab.sh
+++ bin/slab.sh
@@ -16,6 +16,16 @@ case $1 in
         echo "Iniciando ambiente de produção..."
         docker-compose -f ../docker-compose-prod.yaml up
         ;;
+    ubuntu-dev)
+        verificar_root
+        echo "Iniciando ambiente de desenvolvimento..."
+        docker-compose -f ../docker-compose-ubuntu-dev.yaml up
+        ;;
+    ubuntu-prod)
+        verificar_root
+        echo "Iniciando ambiente de produção..."
+        docker-compose -f ../docker-compose-ubuntu-prod.yaml up
+        ;;
     remover_imagens)
         verificar_root
         docker rmi slab_php slab_mysql -f
@@ -31,6 +41,8 @@ case $1 in
         echo "abrir                         Abre o navegador no SLab"
         echo "dev                           Sobe os containers para desenvolvimento do SLab"
         echo "prod                          Sobe os containers para ambiente de produção do SLab"
+        echo "ubuntu-dev                    Sobe os containers para desenvolvimento do SLab usando o container do Ubuntu"
+        echo "ubuntu-prod                   Sobe os containers para ambiente de produção do SLab usando o container do Ubuntu"
         echo "remover_imagens               Remove as imagens do ambiente atual"
         echo "testes                        Executa os testes unitários do PHP"
         echo
