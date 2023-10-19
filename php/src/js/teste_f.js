ceb28d39c3e8a2322e0f83d196243590e7fe7ede
Douglasg2 <116851523+Douglasg2@users.noreply.github.com>
2023-10-18 22:32:27 -0300

    ajuste teste

--- php/src/js/teste_f.js
+++ php/src/js/teste_f.js
@@ -1,58 +1,29 @@
 var ultimoAdicionado = null;
 
-window.onload = function(){
+window.onload = function () {
     ultimoAdicionado = document.getElementById("amostra_a");
 }
 
-function teclaApertada(evt){
+function teclaApertada(evt) {
     if (evt.keyCode === 13) {
-        if (evt.target == ultimoAdicionado){
-            var campo = document.createElement("input");
-            campo.type = "number";
-            campo.className = "form-control";
+        var campo = document.createElement("input");
+        campo.type = "number";
+        campo.className = "form-control";
+
+        if (evt.target.name === "amostra_a[]") {
             campo.name = "amostra_a[]";
-
-            campo.addEventListener("keydown", teclaApertada);
-
             let entradasA = document.getElementById("entradas_a");
             entradasA.appendChild(campo);
-
-            ultimoAdicionado = campo;
-
-            campo.focus();
         } else {
-            ultimoAdicionado.focus();
-        }
-
-        evt.preventDefault();
-    }
-}
-
-var ultimoAdicionadob = null;
-
-window.onload = function(){
-    ultimoAdicionadob = document.getElementById("amostra_b");
-}
-
-function teclaApertada(vta){
-    if (evt.keyCode === 13) {
-        if (evt.target == ultimoAdicionadob){
-            var campo = document.createElement("input");
-            campo.type = "number";
-            campo.className = "form-control";
             campo.name = "amostra_b[]";
-
-            campo.addEventListener("keydown", teclaApertada);
-
-            let entradasB = document.getElementById("entradas_a");
+            let entradasB = document.getElementById("entradas_b");
             entradasB.appendChild(campo);
-
-            ultimoAdicionadob = campo;
-
-        } else {
-            ultimoAdicionadob.focus();
+            
         }
 
+        campo.addEventListener("keydown", teclaApertada);
+        ultimoAdicionado = campo;
+        campo.focus();
         evt.preventDefault();
     }
 }
