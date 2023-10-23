9244b027eec54e589e5556a1e884dfa2d5077eb9
Felipe Rafael Gomes <feliperafael769@gmail.com>
2023-10-22 21:20:26 -0300

    Final tela inicial

--- php/src/js/teste_f.js
+++ php/src/js/teste_f.js
@@ -1,61 +1,62 @@
-var ultimoAdicionadoA = null;
-var ultimoAdicionadoB = null;
+    var ultimoAdicionadoA = null;
+    var ultimoAdicionadoB = null;
 
-window.onload = function(){
-    ultimoAdicionadoA = document.getElementById("amostra_a");
-    ultimoAdicionadoB = document.getElementById("amostra_b");
-}
-
-function teclaApertadaA(evt){
-    if (evt.keyCode === 13) {
-        if (evt.target == ultimoAdicionadoA){
-            if (ultimoAdicionadoA.value.trim() !== "") {
-                var campo = document.createElement("input");
-                campo.type = "number";
-                campo.step = "any"; 
-                campo.className = "form-control";
-                campo.name = "amostra_a[]";
-
-                campo.addEventListener("keydown", teclaApertadaA);
-
-                let entradasA = document.getElementById("entradas_a");
-                entradasA.appendChild(campo);
-
-                ultimoAdicionadoA = campo;
-
-                campo.focus();
-            } else {
-                alert("Campo Amostra A não pode estar vazio!");
-            }
-        }
-        evt.preventDefault();
+    window.onload = function(){
+        ultimoAdicionadoA = document.getElementById("amostra_a");
+        ultimoAdicionadoB = document.getElementById("amostra_b");
     }
-}
 
+    function teclaApertadaA(evt){
+        if (evt.keyCode === 13) {
+            if (evt.target == ultimoAdicionadoA){
+                if (ultimoAdicionadoA.value.trim() !== "") {
+                    var campo = document.createElement("input");
+                    campo.type = "number";
+                    campo.step = "any"; 
+                    campo.className = "form-control";
+                    campo.name = "amostra_a[]";
 
-function teclaApertadaB(evt){
-    if (evt.keyCode === 13) {
-        if (evt.target == ultimoAdicionadoB){
-            if (ultimoAdicionadoB.value.trim() !== "") {
-                var campo = document.createElement("input");
-                campo.type = "number";
-                campo.step = "any"; 
-                campo.className = "form-control";
-                campo.name = "amostra_b[]";
+                    campo.addEventListener("keydown", teclaApertadaA);
 
-                campo.addEventListener("keydown", teclaApertadaB);
+                    let entradasA = document.getElementById("entradas_a");
+                    entradasA.appendChild(campo);
 
-                let entradasB = document.getElementById("entradas_b");
-                entradasB.appendChild(campo);
+                    ultimoAdicionadoA = campo;
 
-                ultimoAdicionadoB = campo;
-
-                campo.focus();
-            } else {
-                alert("Campo Amostra B não pode estar vazio!");
+                    campo.focus();
+                } else {
+                    alert("Campo Amostra A não pode estar vazio!");
+                }
             }
+            evt.preventDefault();
         }
-        evt.preventDefault();
     }
-}
+
+
+    function teclaApertadaB(evt){
+        if (evt.keyCode === 13) {
+            if (evt.target == ultimoAdicionadoB){
+                if (ultimoAdicionadoB.value.trim() !== "") {
+                    var campo = document.createElement("input");
+                    campo.type = "number";
+                    campo.step = "any"; 
+                    campo.className = "form-control";
+                    campo.name = "amostra_b[]";
+
+                    campo.addEventListener("keydown", teclaApertadaB);
+
+                    let entradasB = document.getElementById("entradas_b");
+                    entradasB.appendChild(campo);
+
+                    ultimoAdicionadoB = campo;
+
+                    campo.focus();
+                } else {
+                    alert("Campo Amostra B não pode estar vazio!");
+                }
+            }
+            evt.preventDefault();
+        }
+    }
+
 
